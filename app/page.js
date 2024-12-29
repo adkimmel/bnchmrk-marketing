c3f271320e269afa71d20c94bed6780233fd8c5d
Andrew Kimmel <andrew.d.kimmel@gmail.com>
2024-12-29 15:14:18 -0500

    config

--- app/page.js
+++ app/page.js
@@ -68,20 +68,19 @@ export default async function HomePage() {
 	let locationStates = PLACEHOLDER_STATES;
 
 	try {
-		const [
-			{ ok: countOk, ...fetchedEmployerCount },
-			{ ok: planOk, ...fetchedPlanCounts },
-			filters,
-		] = await Promise.all([
-			employerCountApi({
-				industry: [],
-				state: [],
-				size: [],
-				other: [],
-			}),
-			getTotalPlans(),
-			employerFiltersApi(),
-		]);
+		const [counts, { ok: planOk, ...fetchedPlanCounts }, filters] =
+			await Promise.all([
+				employerCountApi({
+					industry: [],
+					state: [],
+					size: [],
+					other: [],
+				}),
+				getTotalPlans(),
+				employerFiltersApi(),
+			]);
+		console.log("counts=", counts);
+		const { ok: countOk, ...fetchedEmployerCount } = counts;
 
 		if (countOk) employerCount = fetchedEmployerCount;
 		if (planOk) planCounts = fetchedPlanCounts;
--- utils/makeRequest.js
+++ utils/makeRequest.js
@@ -1,42 +1,36 @@
 // utils/makeRequest.js
 
 const makeRequest = async (method, url, body = null) => {
-  // const NEXT_PUBLIC_API_AUTH_TOKEN = process.env.NEXT_PUBLIC_API_AUTH_TOKEN;
-  const NEXT_PUBLIC_API_USERNAME = process.env.NEXT_PUBLIC_API_USERNAME;
-  const NEXT_PUBLIC_API_PASSWORD = process.env.NEXT_PUBLIC_API_PASSWORD;
-  const NEXT_PUBLIC_API_URL = process.env.NEXT_PUBLIC_API_URL;
+	const API_USERNAME = process.env.API_USERNAME;
+	const API_PASSWORD = process.env.API_PASSWORD;
+	const API_URL = process.env.API_URL;
 
-  const headers = new Headers({
-    "Content-Type": "application/json",
-  });
+	const headers = new Headers({ "Content-Type": "application/json" });
 
-  // headers.append("Authorization", `Token ${NEXT_PUBLIC_API_AUTH_TOKEN}`);
-  headers.append(
-    "Authorization",
-    `Basic ${btoa(`${NEXT_PUBLIC_API_USERNAME}:${NEXT_PUBLIC_API_PASSWORD}`)}`
-  );
+	headers.append(
+		"Authorization",
+		`Basic ${btoa(`${API_USERNAME}:${API_PASSWORD}`)}`
+	);
 
-  const fetchOptions = {
-    method,
-    headers,
-    ...(body && { body: JSON.stringify(body) }),
-  };
+	const fetchOptions = {
+		method,
+		headers,
+		...(body && { body: JSON.stringify(body) }),
+	};
 
-  try {
-    const response = await fetch(`${NEXT_PUBLIC_API_URL}${url}`, fetchOptions);
-    const data = await response.json();
+	try {
+		const response = await fetch(`${API_URL}${url}`, fetchOptions);
+		const data = await response.json();
 
-    // If data is an array, wrap it inside an object with a 'list' key
-    if (Array.isArray(data)) {
-      return { list: data, ok: response.ok };
-    }
+		if (Array.isArray(data)) {
+			return { list: data, ok: response.ok };
+		}
 
-    // Otherwise, return the data object as usual
-    return { ...data, ok: response.ok };
-  } catch (error) {
-    console.error("Error in makeRequest:", error.message);
-    return { error: error.message, ok: false };
-  }
+		return { ...data, ok: response.ok };
+	} catch (error) {
+		console.error("Error in makeRequest:", error.message);
+		return { error: error.message, ok: false };
+	}
 };
 
 module.exports = makeRequest;
